8a41e5f0386212d653fbd7b6adcb2af690c7d2d5
Šimon Vacek <simonvacky@email.cz>
2022-03-10 21:11:17 +0100

    added UMLAttribute

--- src/ija/homework1/uml/UMLAttribute.java
+++ src/ija/homework1/uml/UMLAttribute.java
@@ -1,4 +1,37 @@
 package ija.homework1.uml;
 
+/**
+ * Třída reprezentuje atribut, který má své jméno a typ. Je odvozena (rozšiřuje) od třídy Element.
+ * Typ atributu je reprezentován třidou UMLClassifier. Lze použít jako atribut UML třídy nebo argument operace.
+ */
 public class UMLAttribute extends Element {
+    private String name;
+    private UMLClassifier type;
+
+    /**
+     * Vytvoří instanci atributu.
+     * @param name Název atributu.
+     * @param type Typ atributu.
+     */
+    public UMLAttribute(String name, UMLClassifier type) {
+        super(name);
+        this.type = type;
+    }
+
+    /**
+     * Poskytuje informaci o typu atributu.
+     * @return Typ atributu.
+     */
+    public UMLClassifier getType() {
+        return this.type;
+    }
+
+    /**
+     * Vrací řetězec reprezentující stav atributu v podobě "nazev:typ".
+     * @return Řetězec reprezentující atribut.
+     */
+    @Override
+    public java.lang.String toString() {
+        return String.format("%s:%s", this.name, this.type.toString());
+    }
 }
