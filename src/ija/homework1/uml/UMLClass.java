1ffb5f4e076c01efa9984a87ac8db17daed9f77a
Šimon Vacek <simonvacky@email.cz>
2022-03-10 16:02:21 +0100

    implemented UMLClass and UMLClassifier

--- src/ija/homework1/uml/UMLClass.java
+++ src/ija/homework1/uml/UMLClass.java
@@ -1,7 +1,43 @@
 package ija.homework1.uml;
 
 import java.util.Collections;
+import java.util.List;
 
 public class UMLClass extends  UMLClassifier{
+    private String name;
+    private boolean isAbstract;
+    private List<UMLAttribute> attributes;
 
+    public UMLClass(String name) {
+        super(name);
+    }
+
+    public boolean isAbstract() {
+        return this.isAbstract;
+    }
+
+    public void setAbstract(boolean isAbstract) {
+        this.isAbstract = isAbstract;
+    }
+
+    public boolean addAttribute(UMLAttribute attr) {
+        if(this.attributes.contains(attr))
+            return false;
+        return this.attributes.add(attr);
+    }
+
+    public int getAttrPosition(UMLAttribute attr) {
+        return this.attributes.indexOf(attr);
+    }
+
+    public int moveAttrAtPosition(UMLAttribute attr, int pos) {
+        if(!this.attributes.remove(attr))
+            return -1;
+        this.attributes.add(pos, attr);
+        return pos;
+    }
+
+    public java.util.List<UMLAttribute> getAttributes() {
+        return Collections.unmodifiableList(this.attributes);
+    }
 }
--- src/ija/homework1/uml/UMLClassifier.java
+++ src/ija/homework1/uml/UMLClassifier.java
@@ -1,5 +1,27 @@
 package ija.homework1.uml;
 
 public class UMLClassifier extends Element{
+    private String name;
+    private boolean isUserDefined;
 
+    public UMLClassifier(String name) {
+        super(name);
+    }
+
+    public UMLClassifier(String name, boolean isUserDefined) {
+        super(name);
+        this.isUserDefined = isUserDefined;
+    }
+
+    public static UMLClassifier forName(String name) {
+        return new UMLClassifier(name);
+    }
+
+    public boolean isUserDefined() {
+        return this.isUserDefined;
+    }
+
+    public String toString() {
+        return String.format("%s(%s)", this.getName(), isUserDefined ? "true" : "false");
+    }
 }
